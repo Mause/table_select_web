e1a36f90a098e21bd5c6e691cb2ee6ed321c4644
Dominic May <jack.thatch@gmail.com>
2013-07-09 00:13:53 +0800

    Some misc. utils

---
+++ static/js/app/utils.js
@@ -0,0 +1,39 @@
+function get_keys(obj) {
+    'use strict';
+    // Helpers that operate with 'this' within an #each
+    var keys = [];
+
+    function filter_for_own(obj) {
+        var own = [];
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) { own.push(key); }
+        }
+        return own;
+    }
+
+    // var meta = obj[META_KEY], desc = meta && meta.descs[keyName], ret;
+    // if (obj.hasOwnProperty('get')) keys = merge(filter_for_own(obj.get(obj)), keys);
+    // keys = merge(filter_for_own(meta.values), keys);
+    keys = Ember.merge(keys, filter_for_own(obj));
+
+    return Ember.keys(keys);
+}
+
+
+function notif(element, text, timeout) {
+    "use strict";
+    // convenience function \o/
+    $('#myModalLabel').text(text);
+    $('#myModal').modal();
+}
+
+
+Ember.Handlebars.registerHelper('control_smart', function(path, modelPath, options) {
+    // to make sure each table has a unique controller,
+    // and hence a controller that has the right info,
+    // we give it a new controlID each time.
+    // not ideal, but w/e
+    options.hash.controlID = 'control-smart-' + String(options.data.keywords.ball_table.id);
+
+    Ember.Handlebars.helpers.control.call(this, path, modelPath, options);
+});
