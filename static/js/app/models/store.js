b99981c30d3b0d478091acb7826fa6f1c86f0602
Dominic May <jack.thatch@gmail.com>
2013-07-09 15:06:10 +0800

    Monkey patching for better functionality... also seems to strangely break stuff

--- static/js/app/models/store.js
+++ static/js/app/models/store.js
@@ -1,22 +1,82 @@
-DS.RESTAdapter.registerTransform('integer', {
-    deserialize: function(serialized){
-        return serialized;
-    },
-    serialize: function(deserialized){
-        return deserialized;
+var acceptable = [
+    422,
+    400
+];
+
+DS.RESTSerializer.reopen({
+    extractExtendedValidationErrors: function(type, json) {
+        var errors = {};
+        var _this = this;
+
+        function merge(original, updates) {
+            for (var i=0; i<updates.length; i++) {
+                if ('function' === typeof updates[i]) { continue; }
+                original.push(updates[i]);
+            }
+            return original;
+        }
+
+        var attributes = Ember.copy(Ember.get(type, 'attributes'));
+        // attributes = attributes.keys.list;
+        var relationships = Ember.copy(Ember.get(type, 'relationshipsByName'));
+        attributes = merge(attributes.keys.list, relationships.keys.list);
+
+        function get_error(name) {
+            var key = _this._keyForAttributeName(type, name);
+            if (json['errors'].hasOwnProperty(key)) {
+                errors[name] = json['errors'][key];
+            }
+        }
+
+        function get_errors(name) {
+            get_error.call(_this, name);
+            // get_error.call(_this, name + '_id');
+        }
+
+        try {
+            attributes.forEach(get_errors, _this);
+        } catch (e) {
+            console.log(e);
+            debugger;
+        }
+
+        return errors;
     }
 });
 
 
-TableSelectWeb.Store = DS.Store.extend({
-    adapter: DS.RESTAdapter.create({
-        namespace: 'api/v1',
-        // bulkCommit: true,
-        mappings: {
-            ball_tables: TableSelectWeb.BallTable
+DS.RESTAdapter.reopen({
+    didError: function(store, type, record, xhr) {
+        if (acceptable.contains(xhr.status)) {
+
+            debugger;
+            var json = JSON.parse(xhr.responseText),
+                serializer = Ember.get(this, 'serializer'),
+                // errors = serializer.extractValidationErrors(type, json);
+                errors = serializer.extractExtendedValidationErrors(type, json);
+
+            // record.errors = errors;
+
+            // debugger;
+            store.recordWasInvalid(record, errors);
+        } else {
+            console.log('oh?');
+            this._super.apply(this, arguments);
         }
-    }),
-    revision: 12
+    }
 });
 
 
+TableSelectWeb.Adapter = DS.RESTAdapter.create({
+    namespace: 'api/v1',
+
+    mappings: {
+        ball_tables: TableSelectWeb.BallTable,
+        errors: TableSelectWeb.Error
+    }
+}),
+
+TableSelectWeb.Store = DS.Store.extend({
+    adapter: TableSelectWeb.Adapter,
+    revision: 12
+});
