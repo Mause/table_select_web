af63f8cba8abdd70376a06b1b6d516e5705f892d
Dominic May <jack.thatch@gmail.com>
2013-07-09 15:06:26 +0800

    Eh

--- static/js/app/template.js
+++ static/js/app/template.js
@@ -1,16 +1,6 @@
 // compiling on the fly aint the best, but its good enough for what we're doing
 // if this was a fully fledged webapp, i would consider pre-compiling the templates though :P
-templates = [
-    '_attendee_list',
-    '_single_attendee'
-];
 
-for (var i=0; i<templates.length; i++){
-    var source = $('[data-template-name="' + templates[i] + '"]').html();
-    console.assert(source, 'Not found: ' + templates[i]);
-
-    Ember.TEMPLATES[templates[i]] = Ember.Handlebars.compile(source);
-}
 
 // {
 //     getResources: function (path, callback) {
