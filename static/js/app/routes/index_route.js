57ef32f8f32bf77fb7523a579678c00a3b089ce3
Dominic May <jack.thatch@gmail.com>
2013-07-17 17:38:42 +0800

    Updated routes ever so slightly

--- static/js/app/routes/index_route.js
+++ static/js/app/routes/index_route.js
@@ -1,6 +1,12 @@
 TableSelectWeb.IndexRoute = Ember.Route.extend({
     model: function () {
-        return TableSelectWeb.BallTable.find();
+        var records = TableSelectWeb.BallTable.find({});
+
+        // records.then(function(records){
+        //     console.log('then:', records.get('content'));
+        // });
+
+        return records;
     },
 
     renderTemplate: function(controller, model){
--- static/js/app/routes/router.js
+++ static/js/app/routes/router.js
@@ -1,8 +1,9 @@
+TableSelectWeb.Router.reopen({
+    location: 'history'
+});
+
+
 TableSelectWeb.Router.map(function(){
     this.resource('index', {path: '/'});
     this.resource('info', {path: '/info'});
-});
-
-TableSelectWeb.Router.reopen({
-    location: 'history'
 });
