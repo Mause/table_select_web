6597d932263a02da7843f4fc8cddba472d2d4758
Dominic May <jack.thatch@gmail.com>
2013-07-17 18:04:47 +0800

    Model update

--- static/js/app/models/removal_request_model.js
+++ static/js/app/models/removal_request_model.js
@@ -1,6 +1,6 @@
 TableSelectWeb.RemovalRequest = DS.Model.extend({
-    attendee_id: DS.belongsTo('TableSelectWeb.Attendee'),
-    ball_table_id: DS.belongsTo('TableSelectWeb.BallTable'),
+    attendee: DS.belongsTo('TableSelectWeb.Attendee'),
+    ball_table: DS.belongsTo('TableSelectWeb.BallTable'),
     remover_ident: DS.attr('string'),
     state: DS.attr('string')
 });
