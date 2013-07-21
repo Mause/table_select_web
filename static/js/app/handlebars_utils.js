d68f6e9f8f16c91a0e20996da55bb7c93ecb2def
Dominic May <jack.thatch@gmail.com>
2013-07-21 19:11:29 +0800

    Mended control_smart, now uses incrementing variable for controlID

--- static/js/app/handlebars_utils.js
+++ static/js/app/handlebars_utils.js
@@ -1,19 +1,23 @@
-Ember.Handlebars.registerHelper('log_content', function(property, options) {
-  var context = (options.contexts && options.contexts[0]) || this,
-      normalized = Ember.Handlebars.normalizePath(context, property, options.data),
-      pathRoot = normalized.root,
-      path = normalized.path,
-      value = (path === 'this') ? pathRoot : Ember.Handlebars.get(pathRoot, path, options);
+(function(Ember){
+  Ember.Handlebars.registerHelper('log_content', function(property, options) {
+    var context = (options.contexts && options.contexts[0]) || this,
+        normalized = Ember.Handlebars.normalizePath(context, property, options.data),
+        pathRoot = normalized.root,
+        path = normalized.path,
+        value = (path === 'this') ? pathRoot : Ember.Handlebars.get(pathRoot, path, options);
 
-    console.log(value.get('isLoaded'), value.get('content').length);
-});
+      console.log(value.get('isLoaded'), value.get('content').length);
+  });
 
-Ember.Handlebars.registerHelper('control_smart', function(path, modelPath, options) {
-    // to make sure each table has a unique controller,
-    // and hence a controller that has the right info,
-    // we give it a new controlID each time.
-    // not ideal, but w/e
-    options.hash.controlID = 'control-smart-' + String(options.data.keywords.ball_table.id);
 
-    Ember.Handlebars.helpers.control.call(this, path, modelPath, options);
-});
+  var _control_smart_id = 0;
+  Ember.Handlebars.registerHelper('control_smart', function(path, modelPath, options) {
+      // to make sure each table has a unique controller,
+      // and hence a controller that has the right info,
+      // we give it a new controlID each time.
+      // not ideal, but w/e
+      options.hash.controlID = 'control-smart-' + String(_control_smart_id++);
+
+      Ember.Handlebars.helpers.control.call(this, path, modelPath, options);
+  });
+})(Ember);
