c1f728ef973a1f5cc7de8df30fd4f8b729808fa6
Dominic May <jack.thatch@gmail.com>
2013-07-23 01:04:08 +0800

    Added removal_request submitting logic to attendee list controller

--- static/js/app/controllers/attendee_list_controller.js
+++ static/js/app/controllers/attendee_list_controller.js
@@ -4,10 +4,34 @@ TableSelectWeb.AttendeeListController = Ember.Controller.extend({
         return this._super(arguments);
     },
 
-    RequestRemoveAttendee: function() {
+    RequestRemoveAttendee: function(attendee) {
         'use strict';
-        console.log(this.get('target').toString());
-        console.log('Attendee name;', this.get('attendee_name'));
-        console.log('Table num;', this.get('table_num'));
+        // debugger;
+
+        var ball_table = attendee.get('ball_table');
+
+        console.assert(TableSelectWeb.BallTable.detectInstance(ball_table));
+
+        var record_data = {
+            attendee: attendee,
+            ball_table: ball_table,
+            remover_ident: 'unknown',
+            state: 'unresolved'
+        };
+
+        var removal_request = TableSelectWeb.RemovalRequest.createRecord(
+            record_data);
+        var prom = removal_request.save();
+
+        prom.then(function(event){
+            console.assert(attendee.id);
+            debugger;
+            console.log('p1 done');
+            attendee.set('removal_request_exists', true);
+            var prom = attendee.save();
+            prom.then(function(event){
+                console.log('p2 done');
+            });
+        });
     }
 });
