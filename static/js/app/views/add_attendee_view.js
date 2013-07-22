eb0e4541324e1c8693ea03e88dc41324f5b8170d
Dominic May <jack.thatch@gmail.com>
2013-07-23 01:00:31 +0800

    Streamlined add attendee view

--- static/js/app/views/add_attendee_view.js
+++ static/js/app/views/add_attendee_view.js
@@ -2,14 +2,16 @@ TableSelectWeb.AddAttendeeView = Ember.View.extend({
     templateName: 'addAttendee',
     attendee_name: '',
 
-    addAttendeeFormEvent: function(){
+    addAttendeeFormEvent: function(attendee_name){
+        var controller = this.get('controller');
+
         // tell the controller the attendee_name
-        this.set('controller.attendee_name', this.get('attendee_name'));
+        controller.set('attendee_name', attendee_name);
 
         // clear the input field
         this.set('attendee_name', '');
 
         // tell the controller to get its act together
-        this.get('controller').send('addAttendeeEvent');
+        controller.send('addAttendeeEvent');
     }
 });
