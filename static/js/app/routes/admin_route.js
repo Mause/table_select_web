8602ff543ac23b00bbf1358cfffd8b6cfedb8a97
Dominic May <jack.thatch@gmail.com>
2013-07-23 16:51:19 +0800

    Added model to admin route

--- static/js/app/routes/admin_route.js
+++ static/js/app/routes/admin_route.js
@@ -1,4 +1,8 @@
 TableSelectWeb.AdminRoute = Ember.Route.extend({
+    model: function () {
+        return TableSelectWeb.RemovalRequest.find({});
+    },
+
     renderTemplate: function(controller, model){
         this.render({
             outlet: 'application'
