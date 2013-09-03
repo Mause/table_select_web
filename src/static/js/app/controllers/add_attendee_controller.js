b37e4b17b55e720ea9fab030a71898438db7fd54
Dominic May <jack.thatch@gmail.com>
2013-09-03 17:00:36 +0800

    Actions subobject, error handler support code

--- src/static/js/app/controllers/add_attendee_controller.js
+++ src/static/js/app/controllers/add_attendee_controller.js
@@ -1,59 +1,76 @@
 TableSelectWeb.AddAttendeeController = Ember.Controller.extend({
     needs: ['index'],
+    actions: {
 
-    addAttendeeEvent: function(){
-        'use strict';
-        var self = this,
-            ball_table,
-            attendee_name,
-            record_data,
-            attendee,
-            prom;
+        addAttendeeEvent: function(){
+            'use strict';
+            var self = this,
+                ball_table,
+                attendee_name,
+                record_data,
+                attendee,
+                prom;
 
-        // control content is set by the control helper, second arg to helper
-        ball_table = self.get('content');
+            // control content is set by the control helper, second arg to helper
+            ball_table = self.get('content');
 
-        Ember.assert('Not a ball_table instance', TableSelectWeb.BallTable.detectInstance(ball_table));
-        Ember.assert('Bad ball_table id', !!ball_table.id);
+            Ember.assert('Not a ball_table instance', TableSelectWeb.BallTable.detectInstance(ball_table));
+            Ember.assert('Bad ball_table id', !!ball_table.id);
 
-        attendee_name = self.get('attendee_name');
-        this.set('attendee_name', '');
+            attendee_name = self.get('attendee_name');
+            this.set('attendee_name', '');
 
-        attendee_name = attendee_name.trim();
+            attendee_name = attendee_name.trim();
 
-        if (!attendee_name) { return; }
+            if (!attendee_name) { return; }
 
-        record_data = {
-            'attendee_name': attendee_name,
-            'show': true,
-            'ball_table': ball_table
-        };
-        attendee = TableSelectWeb.Attendee.createRecord(record_data);
+            record_data = {
+                'attendee_name': attendee_name,
+                'show': true,
+                'ball_table_id': ball_table
+            };
 
-        console.log('Saving');
-        prom = attendee.save();
+            attendee = this.store.createRecord('attendee', record_data);
+
+            console.log(attendee.ball_table);
+
+            console.log('Saving');
+            // debugger;
+            prom = attendee.save();
 
 
-        var success_handler = function(event) {
-            console.log('success:', event);
-            TableSelectWeb.sendNotification('Attendee add was successful');
+            var success_handler = function(event) {
+                console.log('success:', event);
+                sendNotification('Attendee add was successful');
+            },
+            failure_handler = function(event) {
+                // debugger;
+                var record;
+
+                if (!event.hasOwnProperty('detail')) {
+                    record = event;
+                } else {
+                    record = event.detail;
+                }
+
+                console.log('failed, handling errors', record.errors);
+                self.handle_errors(
+                    record.errors,
+                    self.error_handlers,
+                    record_data);
+            };
+
+            prom.then(success_handler, failure_handler);
         },
-        failure_handler = function(event) {
-            console.log('failed, handling errors', event.detail.errors);
-            self.handle_errors(
-                event.detail.errors,
-                self.error_handlers,
-                record_data);
-        };
-
-        prom.then(success_handler, failure_handler);
     },
 
     error_handlers: {
         attendee_name: function (error, context) {
             if (error.machine === "attendee_exists") {
-                console.info('attendee_exists: %@'.fmt(context.attendee_name));
-                TableSelectWeb.sendNotification(error.human.fmt(context.attendee_name));
+                console.error('attendee_exists: %@'.fmt(context.attendee_name));
+                return {
+                    notification: (error.human.fmt(context.attendee_name))
+                };
             } else {
                 throw new Error('"attendee_name": %@'.fmt(error));
             }
@@ -61,27 +78,16 @@ TableSelectWeb.AddAttendeeController = Ember.Controller.extend({
 
         ball_table_id: function(error, context) {
             if (error.machine === "table_full") {
-                TableSelectWeb.sendNotification(error.human);
+                return {
+                    notification: error.human
+                };
             } else {
                 throw new Error('"ball_table_id": %@'.fmt(error));
             }
         },
 
         show: Ember.K
-    },
-
-    becameError: function() {
-        console.log('standalone becameError:', arguments);
-    },
-
-    becameInvalid: function() {
-        console.log('becameInvalid:', arguments);
-    },
-
-    error: function() {
-        console.log('error:', arguments);
     }
 });
 
-console.assert(!Ember.isNone(TableSelectWeb.ErrorHandlerMixin));
 TableSelectWeb.AddAttendeeController.reopen(TableSelectWeb.ErrorHandlerMixin);
