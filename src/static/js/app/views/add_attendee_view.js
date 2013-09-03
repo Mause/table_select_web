1237e24ee9d089b9c942bfef870c8723d4514fb6
Dominic May <jack.thatch@gmail.com>
2013-09-03 16:58:08 +0800

    Moved actions to actions subobject for Ember 1.0

--- src/static/js/app/views/add_attendee_view.js
+++ src/static/js/app/views/add_attendee_view.js
@@ -2,16 +2,18 @@ TableSelectWeb.AddAttendeeView = Ember.View.extend({
     templateName: 'addAttendee',
     attendee_name: '',
 
-    addAttendeeFormEvent: function(attendee_name){
-        var controller = this.get('controller');
+    actions: {
+        addAttendeeFormEvent: function(attendee_name){
+            var controller = this.get('controller');
 
-        // tell the controller the attendee_name
-        controller.set('attendee_name', attendee_name);
+            // tell the controller the attendee_name
+            controller.set('attendee_name', attendee_name);
 
-        // clear the input field
-        this.set('attendee_name', '');
+            // clear the input field
+            this.set('attendee_name', '');
 
-        // tell the controller to get its act together
-        controller.send('addAttendeeEvent');
+            // tell the controller to get its act together
+            controller.send('addAttendeeEvent');
+        }
     }
 });
