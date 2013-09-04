02989cd4486f4829ffb0934e987926074a5898f7
Dominic May <jack.thatch@gmail.com>
2013-09-04 15:36:09 +0800

    Ember Data 1.0 beta 1

--- src/static/js/app/models/removal_request_model.js
+++ src/static/js/app/models/removal_request_model.js
@@ -1,6 +1,6 @@
 TableSelectWeb.RemovalRequest = DS.Model.extend({
-    attendee: DS.belongsTo('TableSelectWeb.Attendee'),
-    ball_table: DS.belongsTo('TableSelectWeb.BallTable'),
+    attendee: DS.belongsTo('attendee'),
+    ball_table: DS.belongsTo('ball_table'),
     remover_ident: DS.attr('string'),
     state: DS.attr('string')
 });
