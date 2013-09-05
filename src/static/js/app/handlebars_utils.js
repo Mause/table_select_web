ef6701b000e92d726f7c515b5274c7fd45d5d3a7
Dominic May <jack.thatch@gmail.com>
2013-09-05 20:30:06 +0800

    control_smart no longer required, now that we have components

--- src/static/js/app/handlebars_utils.js
+++ src/static/js/app/handlebars_utils.js
@@ -8,16 +8,4 @@
 
         console.log(value.get('isLoaded'), value.get('content').length);
     });
-
-
-    var _control_smart_id = 0;
-    Ember.Handlebars.registerHelper('control_smart', function(path, modelPath, options) {
-        // to make sure each table has a unique controller,
-        // and hence a controller that has the right info,
-        // we give it a new controlID each time.
-        // not ideal, but w/e
-        options.hash.controlID = 'control-smart-' + String(_control_smart_id++);
-
-        Ember.Handlebars.helpers.control.call(this, path, modelPath, options);
-    });
 })();
