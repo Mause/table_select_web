adca94fd70e1c90b46d0b8ce460c14b9436df8b0
Dominic May <jack.thatch@gmail.com>
2013-09-06 20:30:00 +0800

    Cleanup and reorganisation

--- src/assets.py
+++ src/assets.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from itertools import chain
 
 import yaml
 from settings import settings
@@ -7,7 +8,13 @@ from webassets import Environment, Bundle
 from webassets.exceptions import FilterError
 from webassets.filter import Filter, handlebars, register_filter
 
-add_pre_post = lambda pre, post, lst: [pre + thing + post for thing in lst]
+
+DIRNAME = os.path.dirname(__file__)
+STATIC_DIR = os.path.join(DIRNAME, 'static/')
+GLOBAL_FILTERS = None
+
+my_env = Environment(STATIC_DIR, '/static/')
+my_env.debug = settings['release'].upper() == 'DEBUG'
 
 
 class EmberHandlebarsFilter(handlebars.Handlebars, Filter):
@@ -46,6 +53,8 @@ class EmberHandlebarsFilter(handlebars.Handlebars, Filter):
 
 register_filter(EmberHandlebarsFilter)
 
+add_pre_post = lambda pre, post, lst: [pre + thing + post for thing in lst]
+
 
 def parse_subsection(subsection):
     if not subsection:
@@ -71,8 +80,10 @@ def parse_subsection(subsection):
 
     order = subsection['order'] if 'order' in subsection else subsection
 
-    for sub in order:
-        files += parse_subsection(subsection[sub])
+    files += chain.from_iterable(
+        parse_subsection(subsection[sub])
+        for sub in order
+    )
 
     return add_pre_post(prefix, postfix, files)
 
@@ -86,24 +97,20 @@ def sub_bundle(data, key, filters):
     return js_bundle
 
 
-def generate_includes():
-    dirname = os.path.dirname(__file__)
-    with open(os.path.join(dirname, 'includes.yaml')) as fh:
+def generate_js():
+    with open(os.path.join(DIRNAME, 'js_assets.yaml')) as fh:
         data = yaml.load(fh)
 
-    static_dir = os.path.join(dirname, 'static/')
-    my_env = Environment(static_dir, '/static/')
-
-    filters = None
-
     # third party libraries
-    third_party_bundle = sub_bundle(data, 'third_party', filters)
+    third_party_bundle = sub_bundle(data, 'third_party', GLOBAL_FILTERS)
     my_env.register('js_third_party', third_party_bundle)
 
     # application specific
-    app_bundle = sub_bundle(data, 'app', filters)
+    app_bundle = sub_bundle(data, 'app', GLOBAL_FILTERS)
     my_env.register('js_app', app_bundle)
 
+
+def generate_templates():
     # precompiled templates
     handlebars_template_bundle = Bundle(
         'templates/*.hbs',
@@ -113,7 +120,18 @@ def generate_includes():
     )
     my_env.register('handlebars_templates', handlebars_template_bundle)
 
-    my_env.debug = not settings['release'].upper() == 'DEBUG'
+
+def combine_bundles(*bundles):
+    my_env.register('combined',
+                    *bundles,
+                    filters=GLOBAL_FILTERS,
+                    output='gen/combined.js')
+    return my_env['combined'].urls()
+
+
+def gen_assets():
+    generate_js()
+    generate_templates()
 
     if my_env.debug:
         JS_INCLUDES = (
@@ -122,20 +140,13 @@ def generate_includes():
             my_env['handlebars_templates'].urls()
         )
     else:
-        my_env.register('combined',
-                        third_party_bundle,
-                        app_bundle,
-                        handlebars_template_bundle,
-                        filters=filters,
-                        output='gen/combined.js')
+        JS_INCLUDES = combine_bundles(
+            my_env['js_third_party'],
+            my_env['js_app'],
+            my_env['handlebars_templates']
+        )
 
-        JS_INCLUDES = my_env['combined'].urls()
-
-    return JS_INCLUDES
-
-
-def js_includes():
     return '\n'.join(
         '<script src="{}"></script>'.format(filename)
-        for filename in generate_includes()
+        for filename in JS_INCLUDES
     )
