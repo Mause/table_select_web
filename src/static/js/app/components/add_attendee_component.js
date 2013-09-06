eddfa70a104dfaec7d1f0f4a47c485f2280bb76e
Dominic May <jack.thatch@gmail.com>
2013-09-06 22:00:06 +0800

    Removed AddAttendeeController, improved AddAttendeeComponent with sane patterns

--- src/static/js/app/components/add_attendee_component.js
+++ src/static/js/app/components/add_attendee_component.js
@@ -4,17 +4,79 @@ TableSelectWeb.AddAttendeeComponent = Ember.Component.extend({
 
     actions: {
         addAttendeeFormEvent: function(attendee_name){
-            var controller = this.get('controller');
+            'use strict';
+            var self = this,
+                store = this.get('parentView.targetObject.store'),
+                ball_table = self.get('ball_table'),
+                record_data,
+                success_handler,
+                failure_handler;
 
-            // tell the controller the attendee_name
-            controller.set('attendee_name', attendee_name);
-
-            // clear the input field
             this.set('attendee_name', '');
 
-            debugger;
-            // tell the controller to get its act together
-            controller.send('addAttendeeEvent');
-        }
+            attendee_name = attendee_name.trim();
+            if (!attendee_name) { return; }
+
+            Ember.assert('Not a ball_table instance',
+                store.modelFor('ball_table').detectInstance(ball_table));
+            Ember.assert('Bad ball_table id', !!ball_table.id);
+
+            record_data = {
+                'attendee_name': attendee_name,
+                'show': true,
+                'ball_table_id': ball_table
+            };
+
+            console.log('Saving');
+            store.createRecord('attendee', record_data).save().then(
+                success_handler, failure_handler
+            );
+
+            success_handler = function(event) {
+                debugger;
+                console.log('success:', event);
+                sendNotification('Attendee add was successful');
+            };
+
+            failure_handler = function(event) {
+                debugger;
+                var record = event.hasOwnProperty('detail') ? event.detail : event;
+                console.log('failed, handling errors', record.errors);
+
+                self.handle_errors(
+                    record.errors,
+                    self.error_handlers,
+                    record_data);
+            };
+
+        },
+    },
+
+    error_handlers: {
+        attendee_name: function (error, context) {
+            if (error.machine === "attendee_exists") {
+                console.error('attendee_exists: %@'.fmt(context.attendee_name));
+                return {
+                    notification: (error.human.fmt(context.attendee_name))
+                };
+            } else {
+                throw new Error('"attendee_name": %@'.fmt(error));
+            }
+        },
+
+        ball_table_id: function(error, context) {
+            if (error.machine === "table_full") {
+                return {
+                    notification: error.human
+                };
+            } else {
+                throw new Error('"ball_table_id": %@'.fmt(error));
+            }
+        },
+
+        show: Ember.K
     }
 });
+
+
+TableSelectWeb.AddAttendeeComponent.reopen(TableSelectWeb.ErrorHandlerMixin);
