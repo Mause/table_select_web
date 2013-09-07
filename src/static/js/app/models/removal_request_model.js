766cca280402cd1b033297646b283a87e2c66b25
Dominic May <jack.thatch@gmail.com>
2013-09-07 10:00:31 +0800

    Mended removal_request model definition

--- src/static/js/app/models/removal_request_model.js
+++ src/static/js/app/models/removal_request_model.js
@@ -1,6 +1,6 @@
 TableSelectWeb.RemovalRequest = DS.Model.extend({
-    attendee: DS.belongsTo('attendee'),
-    ball_table: DS.belongsTo('ball_table'),
+    attendee_id: DS.belongsTo('attendee'),
+    ball_table_id: DS.belongsTo('ball_table'),
     remover_ident: DS.attr('string'),
     state: DS.attr('string')
 });
