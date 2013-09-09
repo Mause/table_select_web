71689440244627000cb467a51c7a0665db355b63
Dominic May <jack.thatch@gmail.com>
2013-09-09 22:06:16 +0800

    Attempted to comply with the MVC idealogy a little more; still needs work though

--- src/static/js/app/controllers/admin_controller.js
+++ src/static/js/app/controllers/admin_controller.js
@@ -1,1 +1,57 @@
-TableSelectWeb.AdminController = Ember.ArrayController.extend({});
+TableSelectWeb.AdminController = Ember.ArrayController.extend({
+    actions: {
+        action: function(records, state, sh){
+            'use strict';
+            var view = this.get('view'),
+                promises,
+                success_note,
+                success_submit,
+                failure;
+
+            records.forEach(function(record){
+                record.set('state', state);
+                if (sh == 'show' && record.get('attendee.show') !== true) {
+                    record.set('attendee.show', true);
+                } else if (sh == 'hide' && record.get('attendee.show') !== false) {
+                    record.set('attendee.show', false);
+                }
+            });
+
+            debugger;
+            if (records.length == 1){
+                records[0].save().then(
+                    success_submit, failure);
+            } else {
+                promises = records.invoke('save');
+                Ember.RSVP.all(promises).then(
+                    success_submit, failure
+                );
+            }
+
+            success_submit = function(requested){
+                debugger;
+                var attendees = [],
+                    proms;
+                requested.forEach(function(removal_request){
+                    debugger;
+                    var attendee = removal_request.get('attendee');
+                    attendee.set('removal_request_exists', true);
+                });
+                proms = attendees.invoke('save');
+                Ember.RSVP.all(proms).then(
+                    success_note, failure
+                );
+            };
+
+            success_note = function(attendees){
+                self.clear_checkboxes();
+                sendNotification('Success');
+            };
+
+            failure = function(){
+                debugger;
+                sendNotification('Failure');
+            };
+        }
+    }
+});
