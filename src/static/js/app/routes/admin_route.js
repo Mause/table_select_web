00990aa72aea98dc49943bb1b4d2664bfa3c0028
Dominic May <jack.thatch@gmail.com>
2013-09-13 20:00:21 +0800

    Refactored out renderTemplate, added beforeModel
     * Refactored out renderTemplate
     * added beforeModel hook to ensure the user is authenticated before trying to load removal_requests

--- src/static/js/app/routes/admin_route.js
+++ src/static/js/app/routes/admin_route.js
@@ -1,13 +1,13 @@
-TableSelectWeb.AdminRoute = Ember.Route.extend({
+TableSelectWeb.AdminRoute = Ember.Route.extend(TableSelectWeb.ApplicationRouteMixin, {
+    beforeModel: function (){
+        if (!TableSelectWeb.AuthManager.isAuthenticated()){
+            this.transitionTo('index');
+        }
+    },
+
     model: function () {
         return this.get('store').findQuery('removal_request', {
             'state': 'unresolved'
-        });
-    },
-
-    renderTemplate: function(controller, model){
-        this.render({
-            outlet: 'application'
         });
     }
 });
