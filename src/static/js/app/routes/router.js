7fdecada311b02ce335921a708583420b69bfee2
Dominic May <jack.thatch@gmail.com>
2013-09-13 20:04:44 +0800

    Refactor, added isAuthenticated property
     * Refactored out renderTemplate to TableSelectWeb.ApplicationRouteMixin
     * added isAuthenticated property :)

--- src/static/js/app/routes/router.js
+++ src/static/js/app/routes/router.js
@@ -1,5 +1,14 @@
 TableSelectWeb.Router.reopen({
-    location: 'history'
+    location: 'history',
+
+    init: function(){
+        TableSelectWeb.AuthManager = AuthManager.create();
+        return this._super.apply(this, arguments);
+    },
+
+    isAuthenticated: function() {
+        return TableSelectWeb.AuthManager.isAuthenticated();
+    }.property('TableSelectWeb.AuthManager.apiKey')
 });
 
 TableSelectWeb.Router.map(function(){
@@ -7,4 +16,12 @@ TableSelectWeb.Router.map(function(){
     this.resource('info', {path: '/info'});
     this.resource('admin', {path: '/admin'});
     this.resource('auth', {path: '/auth'});
+});
+
+TableSelectWeb.ApplicationRouteMixin = Ember.Mixin.create({
+    renderTemplate: function(controller, model){
+        this.render({
+            outlet: 'application'
+        });
+    }
 });
