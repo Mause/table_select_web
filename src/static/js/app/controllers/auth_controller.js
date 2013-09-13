2fa76f04c0aa85a4be99ea6d1afb1c346a95ec98
Dominic May <jack.thatch@gmail.com>
2013-09-13 20:06:20 +0800

    Added login logic

--- src/static/js/app/controllers/auth_controller.js
+++ src/static/js/app/controllers/auth_controller.js
@@ -5,11 +5,31 @@ TableSelectWeb.AuthController = Ember.ArrayController.extend(Ember.Evented, {
     actions: {
         submitAuthFormEvent: function(){
             var username = this.get('username'),
-                password = this.get('password');
+                password = this.get('password'),
+                adapter = this.store.adapterFor({typeKey: 'me'}),
+                url = adapter.buildURL('me'),
+                self = this;
 
-            this.send('login', {
+            var data = {
                 username: username,
                 password: password
+            };
+
+            adapter.ajax(url, 'POST', {data: data}).then(function(data){
+                TableSelectWeb.AuthManager.authenticate(
+                    data.api_key.access_token,
+                    data.api_key.user_id);
+                self.transitionToRoute('/');
+
+            }, function(xhr){
+                debugger;
+                if (xhr.status === 401) {
+                    this.set('password', '');
+                    sendNotification('Invalid login credentials');
+                } else {
+                    TableSelectWeb.AuthManager.reset();
+                    console.log(arguments);
+                }
             });
         }
     }
