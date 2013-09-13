61199f595be2a9a0337fbd5621f6322052ce9f3c
Dominic May <jack.thatch@gmail.com>
2013-09-13 20:11:44 +0800

    Added key logic, is_authorized logic
     * Added key creation & verification logic
     * logic for determining if a user if authorized/authenticated

--- src/ember_data.py
+++ src/ember_data.py
@@ -1,6 +1,12 @@
+import re
 import json
+import time
+import hmac
+import base64
 import logging
 from contextlib import closing
+
+from settings import settings
 from utils import (
     dict_from_query,
     pluralize,
@@ -8,13 +14,67 @@ from utils import (
     get_primary_key_name_from_table
 )
 
+SEP = str(base64.b64encode(b'DOM'))[2:-1]
+AUTH_RE = re.compile(r'(\w+) (.*)')
+API_KEY_SEED = settings['api_key_seed'].encode('utf-8') or b'bytes'
+
 
 class AuthorizedEndpoint(object):
     def is_authorized(self):
         if 'Authorization' not in self.headers:
+            logging.info('Attempted to access restricted endpoint without authentication')
             return False
         else:
             auth_data = self.headers['Authorization']
+            match = AUTH_RE.match(auth_data)
+            if match:
+                auth_type = match.groups()[0]
+                if auth_type == 'none':
+                    return False
+                elif auth_type == 'Bearer':
+                    key = match.groups()[1]
+
+                    return self.verify_key(key)
+                else:
+                    self.set_bad_error(401)
+
+    def verify_key(self, key):
+        user_hash, timestamp = key.split(SEP)
+
+        timestamp = base64.b64decode(timestamp.encode('utf-8'))
+        timestamp = float(timestamp.decode('utf-8'))
+
+        possible_users = set()
+        for username in settings['auth_combos'].keys():
+            possible_users.add(
+                hmac.new(API_KEY_SEED, username.encode('utf-8')).hexdigest()
+            )
+
+        if timestamp > time.time():
+            logging.info('Invalid key, timestamp in future')
+            return False
+
+        elif user_hash not in possible_users:
+            logging.info('No such user hash')
+            return False
+
+        else:
+            return True
+
+    def create_key(self, username):
+        user_hash = hmac.new(API_KEY_SEED, username).hexdigest()
+
+        timestamp = str(base64.b64encode(str(time.time()).encode('utf-8')))[2:-1]
+
+        ret = '{}{}{}'.format(
+            user_hash,
+            SEP,
+            timestamp
+        )
+        if "b'" in ret:
+            import code
+            code.interact(local=locals())
+        return ret
 
 
 class BaseRESTEndpoint(AuthorizedEndpoint):
