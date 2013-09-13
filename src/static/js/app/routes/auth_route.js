ea2d8bb12679c1ba5da861c6d37c974fcaa3f7b1
Dominic May <jack.thatch@gmail.com>
2013-09-13 20:02:19 +0800

    Added beforModel hook to redirect user to homepage if already auth'd

--- src/static/js/app/routes/auth_route.js
+++ src/static/js/app/routes/auth_route.js
@@ -1,7 +1,7 @@
-TableSelectWeb.AuthRoute = Ember.Route.extend({
-    renderTemplate: function(controller, model){
-        this.render({
-            outlet: 'application'
-        });
+TableSelectWeb.AuthRoute = Ember.Route.extend(TableSelectWeb.ApplicationRouteMixin, {
+    beforeModel: function(){
+        if (TableSelectWeb.AuthManager.isAuthenticated()) {
+            this.transitionTo('/');
+        }
     }
 });
