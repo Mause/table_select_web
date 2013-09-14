a32a4ef43c3a5387f9b73633334ab6f0b970b6be
Dominic May <jack.thatch@gmail.com>
2013-09-14 22:59:39 +0800

    DRY slightly

--- src/settings.py
+++ src/settings.py
@@ -3,5 +3,9 @@ import json
 with open(os.path.join(os.path.dirname(__file__), 'settings.json')) as fh:
     settings = json.load(fh)
 
+settings['DATABASE_URL'] = (
+    os.environ.get("DATABASE_URL", settings.get('DATABASE_URL')))
+
+
 if 'HEROKU' in os.environ:
     settings['release'] = 'PRODUCTION'
