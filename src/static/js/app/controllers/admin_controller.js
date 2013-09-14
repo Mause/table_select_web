2d4c8d497b5b674bdca86174a820e35db227448a
Dominic May <jack.thatch@gmail.com>
2013-09-14 18:36:57 +0800

    Mended admin page callbacks :P

--- src/static/js/app/controllers/admin_controller.js
+++ src/static/js/app/controllers/admin_controller.js
@@ -1,57 +1,53 @@
 TableSelectWeb.AdminController = Ember.ArrayController.extend(Ember.Evented, {
+    success_submit: function(requested){
+        debugger;
+        var attendees = [],
+            proms;
+
+        requested.forEach(function(removal_request){
+            var attendee = removal_request.get('attendee');
+            attendees.push(
+                attendee.set('removal_request_exists', true)
+            );
+        });
+
+        proms = attendees.invoke('save');
+        Ember.RSVP.all(proms).then(
+            this.success_note, this.failure
+        );
+    },
+
+    success_note: function(attendees){
+        debugger;
+        this.trigger('clear_checkboxes');
+        sendNotification('Success');
+    },
+
+    failure: function(){
+        debugger;
+        sendNotification('Failure');
+    },
+
     actions: {
         action: function(records, state, sh){
             'use strict';
-            var view = this.get('view'),
-                promises,
-                success_note,
-                success_submit,
-                failure;
+            debugger;
+
+            var promises;
 
             records.forEach(function(record){
                 record.set('state', state);
                 record.set('attendee.show', sh == 'show');
             });
 
-            debugger;
-            if (records.length == 1){
-                records[0].save().then(
-                    success_submit, failure);
-            } else {
-                promises = records.invoke('save');
-                Ember.RSVP.all(promises).then(
-                    success_submit, failure
-                );
-            }
+            console.assert(this.success_submit);
+            console.assert(this.failure);
+            console.assert(this.success_note);
 
-            success_submit = function(requested){
-                debugger;
-                var attendees = [],
-                    proms;
-
-                requested.forEach(function(removal_request){
-                    debugger;
-                    var attendee = removal_request.get('attendee');
-                    attendee.set('removal_request_exists', true);
-
-                });
-
-                proms = attendees.invoke('save');
-                Ember.RSVP.all(proms).then(
-                    success_note, failure
-                );
-            };
-
-            success_note = function(attendees){
-                debugger;
-                this.trigger('clear_checkboxes');
-                sendNotification('Success');
-            };
-
-            failure = function(){
-                debugger;
-                sendNotification('Failure');
-            };
+            promises = records.invoke('save');
+            Ember.RSVP.all(promises).then(
+                this.success_submit, this.failure
+            );
         }
     }
 });
