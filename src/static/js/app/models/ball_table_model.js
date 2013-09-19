fbf7542fa1bbf0337eb2563e1674d0e9c1d69a2d
Dominic May <jack.thatch@gmail.com>
2013-09-19 22:06:37 +0800

    Displayed only those attendees with their show attribute equal to true

--- src/static/js/app/models/ball_table_model.js
+++ src/static/js/app/models/ball_table_model.js
@@ -2,7 +2,13 @@ TableSelectWeb.BallTable = DS.Model.extend({
     ball_table_name: DS.attr('string'),
     full: DS.attr('boolean'),
     ball_table_num: DS.attr('number'),
-    attendees: DS.hasMany('attendee', {embedded: 'load', async: true}),
+    attendees: DS.hasMany('attendee', {async: true}),
+
+    displayable_attendees: function(){
+        return this.get('attendees').filter(function(attendee){
+            return attendee.get('show') === true;
+        });
+    }.property('attendees.@each.show').cacheable(),
 
     row_end: function() {
         // this is for display purposes only
@@ -11,8 +17,4 @@ TableSelectWeb.BallTable = DS.Model.extend({
 
         return row_end;
     }.property('ball_table_num').cacheable(),
-
-    displayable_attendees: function(){
-        return this.get('attendees').filterBy('show', true);
-    }.property('attendees').cacheable()
 });
