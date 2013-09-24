52d3d695ca45f7a3a2555b6ffec86df1169e3c4d
Dominic May <jack.thatch@gmail.com>
2013-09-24 17:36:27 +0800

    Removed unneeded function, no longer needed with new error pattern

--- src/static/js/app/models/serializer.js
+++ src/static/js/app/models/serializer.js
@@ -1,42 +1,4 @@
 var ApplicationSerializer = DS.RESTSerializer.extend({
-    extractExtendedValidationErrors: function(type, json) {
-        var errors = {};
-        var _this = this;
-
-        function merge(original, updates) {
-            for (var i=0; i<updates.length; i++) {
-                if ('function' === typeof updates[i]) { continue; }
-                original.push(updates[i]);
-            }
-            return original;
-        }
-
-        var attributes = Ember.get(type, 'attributes').copy();
-        var relationships = Ember.get(type, 'relationshipsByName').copy();
-        attributes = merge(attributes.keys.list, relationships.keys.list);
-
-        function get_error(name) {
-            var key = _this._keyForAttributeName(type, name);
-            if (json.errors.hasOwnProperty(key)) {
-                errors[name] = json.errors[key];
-            }
-        }
-
-        function get_errors(name) {
-            get_error.call(_this, name);
-            get_error.call(_this, name + '_id');
-        }
-
-        try {
-            attributes.forEach(get_errors, _this);
-        } catch (e) {
-            console.log(e);
-            debugger;
-        }
-
-        return errors;
-    },
-
     // taken from the TRANSITION.md file for Ember.js
     normalize: function(type, hash, property) {
         var normalized = {}, normalizedProp;
