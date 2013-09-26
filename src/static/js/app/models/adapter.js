e6172e39c8cac932b0e780fdd727171d66b854a6
Dominic May <jack.thatch@gmail.com>
2013-09-26 17:03:09 +0800

    Cleanup, remove need for IIFE

--- src/static/js/app/models/adapter.js
+++ src/static/js/app/models/adapter.js
@@ -1,31 +1,27 @@
-(function(Ember){
-    'use strict';
-    var acceptable = [
+DS.RESTAdapter.reopen({
+    ajaxError: function(jqXHR) {
+        var error = this._super(jqXHR);
+
+        if (jqXHR && this.acceptable.contains(jqXHR.status)) {
+            var json = JSON.parse(jqXHR.responseText);
+
+            return new DS.InvalidError(json["errors"]);
+        } else {
+            return error;
+        }
+    },
+    headers: {},
+    acceptable: [
         422,
         400
-    ];
+    ]
+});
 
-    DS.RESTAdapter.reopen({
-        ajaxError: function(jqXHR) {
-            var error = this._super(jqXHR);
+TableSelectWeb.ApplicationAdapter = DS.RESTAdapter.extend({
+    namespace: 'api/v1',
 
-            if (jqXHR && acceptable.contains(jqXHR.status)) {
-                console.log('Acceptable!', jqXHR.status);
-                var json = JSON.parse(jqXHR.responseText);
-                return new DS.InvalidError(json["errors"]);
-            } else {
-                return error;
-            }
-        },
-        headers: {}
-    });
-
-    TableSelectWeb.ApplicationAdapter = DS.RESTAdapter.extend({
-        namespace: 'api/v1',
-
-        mappings: {
-            ball_tables: TableSelectWeb.BallTable,
-            removal_request: TableSelectWeb.RemovalRequest
-        }
-    });
-})(Ember);
+    mappings: {
+        ball_tables: TableSelectWeb.BallTable,
+        removal_request: TableSelectWeb.RemovalRequest
+    }
+});
