4ffa110a5fca83892e897c9f23272a5ea0b981f4
Dominic May <jack.thatch@gmail.com>
2013-09-26 16:29:10 +0800

    Cleanup

--- src/settings.py
+++ src/settings.py
@@ -6,10 +6,10 @@ with open(os.path.join(os.path.dirname(__file__), 'settings.json')) as fh:
 settings['DATABASE_URL'] = (os.environ.get("DATABASE_URL",
                                            settings.get('DATABASE_URL')))
 
-flags = {
-    'is_production': lambda: 'HEROKU' in os.environ,
-    'is_staging': lambda: 'STAGING' in os.environ,
-    'is_debug': lambda: not flags.is_production()
-}
 
-flags = type('flags', (object,), flags)
+class Flags(object):
+    is_production = lambda self=None: 'HEROKU' in os.environ
+    is_staging = lambda self=None: 'STAGING' in os.environ
+    is_debug = lambda self=None: not flags.is_production()
+
+flags = Flags()
