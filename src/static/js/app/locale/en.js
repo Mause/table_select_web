f96ca6e5e8fae72c9b5f9ea63d9c15e4a662b8b9
Dominic May <jack.thatch@gmail.com>
2013-09-27 22:13:22 +0800

    Grammar

--- src/static/js/app/locale/en.js
+++ src/static/js/app/locale/en.js
@@ -2,8 +2,8 @@ Ember.STRINGS = {
     unauthorized: 'You are not authorized',
     table_full: 'That table is full',
     attendee_exists: 'Attendee "%@" already exists',
-    attendee_add_success: 'Attendee add was successful',
+    attendee_add_success: 'Attendee addition was successful',
     invalid_login: 'Invalid login credentials',
     removal_request_submit_success: 'Removal request successfully submitted',
-    removal_request_submit_failure: 'Removal request submit failure'
+    removal_request_submit_failure: 'Removal request submission failure'
 };
