1037fb7ca71862326134aa3aaf866614346b7218
Dominic May <jack.thatch@gmail.com>
2013-10-04 16:49:11 +0800

    Make sure to unload all removal request models on logout, and some comments

--- src/static/js/app/routes/application_route.js
+++ src/static/js/app/routes/application_route.js
@@ -2,7 +2,13 @@ TableSelectWeb.ApplicationRoute = Ember.Route.extend({
     actions: {
         logout: function(){
             'use strict';
+            // remove the authentication data
             TableSelectWeb.AuthManager.reset();
+
+            // remove all the removal_request's from memory
+            this.store.unloadAll('removal_request');
+
+            // and redirect to the homepage
             this.transitionTo('/');
         }
     }
